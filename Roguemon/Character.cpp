166f5a04344531166a8d627bab745a91c2eaee45
SqrtPapere <francesco.pegoraro@hotmail.it>
2016-03-22 18:48:37 +0100

    rimosso robaccia

--- Roguemon/Character.cpp
+++ Roguemon/Character.cpp
@@ -1,9 +1,3 @@
-//
-//  Character.cpp
-//  Roguemon
-//
-//  Created by Guido Lippi on 22/03/16.
-//  Copyright © 2016 Pegolippi. All rights reserved.
-//
+
 
 #include "Character.hpp"
--- Roguemon/Character.hpp
+++ Roguemon/Character.hpp
@@ -1,10 +1,4 @@
-//
-//  Character.hpp
-//  Roguemon
-//
-//  Created by Guido Lippi on 22/03/16.
-//  Copyright © 2016 Pegolippi. All rights reserved.
-//
+
 
 #ifndef Character_hpp
 #define Character_hpp
